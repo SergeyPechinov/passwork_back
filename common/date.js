ddb521adb9286f9173e5bf3f1528148c1997f7c6
Sergey Pechinov <sen1c163rus@gmail.com>
2020-01-15 21:20:06 +0400

    update time jwt and fix date

--- common/date.js
+++ common/date.js
@@ -7,7 +7,7 @@ const
 
 		year = date.getFullYear(),
 		day = String(date.getDate()).length === 1 ? `0${date.getDate()}` : date.getDate(),
-		month = String(date.getMonth()).length === 1 ? `0${date.getMonth()}` : date.getMonth(),
+		month = String(date.getMonth()).length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1,
 		hours = String(date.getHours()).length === 1 ? `0${date.getHours()}` : date.getHours(),
 		minutes = String(date.getMinutes()).length === 1 ? `0${date.getMinutes()}` : date.getMinutes(),
 		seconds = String(date.getSeconds()).length === 1 ? `0${date.getSeconds()}` : date.getSeconds(),
--- common/jwt/generateJWT.js
+++ common/jwt/generateJWT.js
@@ -1,10 +1,12 @@
 const {generateJWT} = require('./jwt');
 
 const generateAccess = email => {
+	// return generateJWT(email, 5);
 	return generateJWT(email, 60 * 60);
 };
 
 const generateRefresh = email => {
+	// return generateJWT(email, 30);
 	return generateJWT(email, 60 * 60 * 24 * 30);
 };
 
